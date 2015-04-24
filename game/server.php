1df31938cdb5136f4c960a7310152fbc6f6ee7c1
Marco Chen <zhijie2008.chen@gmail.com>
2015-04-24 17:06:49 +0800

    更改地址

--- game/server.php
+++ game/server.php
@@ -4,7 +4,7 @@ session_start();
 $game_key = 'demo-game-2';
 $game_secret = 'demo-game-2-secret';
 
-$checkTicketUrl = 'http://gc.dev.gc.ghome.com/user/getticketuserinfo';
+$checkTicketUrl = 'http://gc.dev.gc.hgame.com/user/getticketuserinfo';
 /**
  * 生成随机数方法
  * @param $n int 随机数位置
