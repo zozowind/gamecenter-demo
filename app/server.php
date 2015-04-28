e3ffc2cd064c0a7e431bdf0d69fa4396ac76873d
Marco Chen <zhijie2008.chen@gmail.com>
2015-04-28 11:34:28 +0800

    添加agent

--- app/server.php
+++ app/server.php
@@ -82,7 +82,7 @@
             );
 
             $result = httpRequestJson($payConfirmUrl, signTheData($data,SECRET_KEY));
-            return $result;
+            echo $result;
             exit;
             break;
 
@@ -231,8 +231,9 @@
     function httpRequestJson($url, $data){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$url);
-        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST,1);
         curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
